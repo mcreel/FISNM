cdad2edd8484c91b70143e839f80e48abfc363f5
JLDC <jonathan.chassot@unisg.ch>
2022-08-09 13:23:55 +0200

    Add cleaner verbosity and loss tracking

--- GARCH/neuralnets.jl
+++ GARCH/neuralnets.jl
@@ -3,7 +3,13 @@ using StatsBase
 
 # Some helpers
 tabular2rnn(X) = [view(X, i:i, :) for i ∈ 1:size(X, 1)]
-rmse_loss(Ŷ, Y) = sqrt(mean(abs2.(Ŷ - Y)))
+
+# In the following losses, Ŷ is always the sequence of predictions
+# RMSE on last item
+rmse_loss(Ŷ, Y) = sqrt(mean(abs2.(Ŷ[end] - Y)))
+# MSE on full sequence predicton
+mse_full(Ŷ, Y) = sum(mean(abs2.(ŷᵢ - Y) for ŷᵢ ∈ Ŷ))
+# ------------------------------------------------------------------------------------------
 
 # Create a neural network according to chosen specification
 function build_net(;
@@ -71,14 +77,22 @@ bilstm_net(n_hidden, dev=cpu) =
 # Trains a recurrent neural network
 function train_rnn!(
     m, opt, dgp, n, S, dtY; 
-    epochs=100, batchsize=32, dev=cpu, loss=rmse_loss
+    epochs=100, batchsize=32, dev=cpu, loss=mse_full,
+    validation_loss=true, verbosity=1
 )
     Flux.trainmode!(m) # In case we have dropout / batchnorm
     θ = Flux.params(m) # Extract parameters
-
+    
+    # Create a validation set to compute and keep track of losses
+    if validation_loss
+        Xv, Yv = map(dev, dgp(n, S))
+        StatsBase.transform!(dtY, Yv)
+        Xv = tabular2rnn(Xv)
+        losses = zeros(epochs)
+    end
+    
     # Iterate over training epochs
     for epoch ∈ 1:epochs
-        println("epoch $epoch of $epochs")
         X, Y = map(dev, dgp(n, S)) # Generate a new batch
         # Standardize targets for MSE scaling
         # no need to do this for every sample, use a high accuracy
@@ -92,15 +106,32 @@ function train_rnn!(
             Xb, Yb = tabular2rnn(X[:, idx]), Y[:, idx]
             # Compute loss and gradients
             ∇ = gradient(θ) do
-                # don't use first, to warm up state
-                m(Xb[1])
-                err = [abs2.(Yb - m(x)) for x ∈ Xb[2:end]]
-                sum(sum(err))/n
+                m(Xb[1]) # don't use first, to warm up state
+                Ŷ = [m(x) for x ∈ Xb[2:end]]
+                loss(Ŷ, Yb)
             end
             Flux.update!(opt, θ, ∇) # Take gradient descent step
         end
+
+        # Compute validation loss and print status if verbose
+        if validation_loss
+            Flux.reset!(m)
+            Flux.testmode!(m)
+            m(Xv[1]) # Warm up state on first observation
+            Ŷ = [m(x) for x ∈ Xv[2:end]]
+            losses[epoch] = loss(Ŷ, Yv)
+            Flux.trainmode!(m)
+            epochs % verbosity == 0 && @info "$epoch / $epochs" losses[epoch]
+        else
+            epochs % verbosity == 0 && @info "$epoch / $epochs"
+        end
     end
-    nothing
+    # Return losses if tracked
+    if validation_loss
+        losses
+    else
+        nothing
+    end
 end
 
 # Trains a bidirectional neural network
