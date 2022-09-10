f0bdab12327821cef860874e56a1e0e6753d2351
Michael Creel <michael.creel@uab.es>
2022-09-10 08:34:23 +0200

    MA2 and Logit dgps are ready

--- MA2/MA2lib.jl
+++ MA2/MA2lib.jl
@@ -14,9 +14,32 @@ function PriorDraw()
     [θ1, θ2]
 end
 
+# get a set of draws from prior
+function PriorDraw(n)
+    draws = zeros(2,n)
+    for i = 1:n
+        draws[:,i] .= PriorDraw()
+    end
+    draws
+end
+
 function ma2(θ, n)
     e = randn(n+2)
     e[3:end] .+ θ[1].*e[2:end-1] .+ θ[2].*e[1:end-2]
 end
+
+    
+# generates S samples of length n
+# returns are:
+# x: 1 X S*n vector of data from EGARCH model
+# y: 2 X S*n vector of parameters used to generate each sample
+@views function dgp(n, S)
+    y = PriorDraw(S)     # the parameters for each sample
+    x = zeros(1, n*S)    # the Garch data for each sample
+    for s = 1:S
+        x[:,n*s-n+1:s*n] = ma2(y[:,s], n)
+    end
+    Float32.(x), Float32.(y)
+end    
 
 
