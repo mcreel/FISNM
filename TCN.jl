ada36fb03962beda1538ebebc2eca8df1a6d9950
JLDC <jonathan.chassot@unisg.ch>
2022-09-12 21:38:12 +0200

    Remove Dropout from TCN

--- TCN.jl
+++ TCN.jl
@@ -12,7 +12,7 @@
 # Temporal blocks which compose the layers of the TCN
 function TemporalBlock(
     chan_in::Int, chan_out::Int; 
-    dilation::Int, kernel_size::Int, dropout_rate::AbstractFloat = 0.2,
+    dilation::Int, kernel_size::Int,
     residual::Bool = true, pad = SamePad()
 )
     # Causal convolutions
@@ -20,11 +20,9 @@ function TemporalBlock(
         Conv((1, kernel_size), chan_in => chan_out, dilation = dilation, 
             pad = pad),
         BatchNorm(chan_out, relu),
-        Dropout(dropout_rate),
         Conv((1, kernel_size), chan_out => chan_out, dilation = dilation, 
             pad = pad),
         BatchNorm(chan_out, relu),
-        Dropout(dropout_rate)
     )
     residual || return causal_conv
     # Skip connection (residual net)
@@ -44,10 +42,10 @@ end
 function TCN(
     channels::AbstractVector{Int}; 
     kernel_size::Int, dilation_factor::Int = 2, 
-    dropout_rate::AbstractFloat = 0.2, residual::Bool = true, pad = SamePad()
+    residual::Bool = true, pad = SamePad()
 )
     Chain([TemporalBlock(chan_in, chan_out, dilation = dilation_factor ^ (i - 1), 
-        kernel_size = kernel_size, dropout_rate = dropout_rate, residual = residual,
+        kernel_size = kernel_size, residual = residual,
         pad = pad) 
         for (i, (chan_in, chan_out)) ∈ enumerate(zip(channels[1:end-1], channels[2:end]))]...)
 end
