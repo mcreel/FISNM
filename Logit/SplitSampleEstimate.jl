da8a29dd3ec49d8a7ff4666ff3c7a21c85e08575
Michael Creel <michael.creel@uab.es>
2022-09-22 14:26:20 +0200

    split sample improves RMSE of larger samples

--- Logit/SplitSampleEstimate.jl
+++ Logit/SplitSampleEstimate.jl
@@ -2,45 +2,73 @@ using Plots, Random, BSON, Optim
 include("LogitLib.jl")
 include("neuralnets.jl")
 
-function main()
-thisrun = "working"
+@views function main()
+whichrun = "working"
 # General parameters
-MCreps = 1000 # Number of Monte Carlo samples for each sample size
-N = [50, 100, 150]  # Sample sizes (most useful to incease by 4X)
+MCreps = 5000 # Number of Monte Carlo samples for each sample size
 MCseed = 77
 
-BSON.@load "bestmodel_50.bson" m
-
-# NNet estimation (nnet object must be pre-trained!)
-err_nnet = zeros(3, MCreps, length(N))
-BC = zeros(3, length(N))
-Random.seed!(trainseed) # avoid sample contamination for NN training
-Threads.@threads for i = 1:size(N,1)
+# use a model trained with samples of size n
+# to fit longer samples
+base_n = 200
+k = 3 # number of parameters
+BSON.@load "bestmodel_$base_n.bson" m
+BSON.@load "bias_correction.bson" BC N
+N = [100, 200, 400, 800, 1600]
+err_nnet = zeros(k, MCreps, length(N))
+# loop over sample sizes
+for i = 1:size(N,1)
     n = N[i]
-
-    # bias correction
-    Random.seed!(BCseed)
-    X, Y = dgp(n, 100000) # Generate data according to DGP
-    X = batch_timeseries(X, n, n) # Transform to rnn format
-    Flux.testmode!(m) # In case nnet has dropout / batchnorm
-    Flux.reset!(m)
-    m(X[1]) # warmup
-    Yhat = [m(x) for x ∈ X[2:end]][end]
-    BC[:,i] = mean(Yhat-Y, dims=2)
-    # final fit and errors
     Random.seed!(MCseed)
-    X, Y = dgp(n, MCreps) # Generate data according to DGP
-    X = batch_timeseries(X, n, n) # Transform to rnn format
-    # Get NNet fit, with bias correction
-    Flux.testmode!(m) # In case nnet has dropout / batchnorm
-    Flux.reset!(m)
-    m(X[1]) # warmup
-    Yhat = [m(x) for x ∈ X[2:end]][end] .- BC[:,i]
-    err_nnet[:, :, i] = Yhat - Y 
-    # Save model as BSON
-    println("Neural network, n = $n done.")
+    Yhat = zeros(k, MCreps)
+    Y = similar(Yhat)
+    # loop over MC reps, for each sample size
+    for rep = 1:MCreps
+        x, y = dgp(n, 1)
+        Y[:,rep] = y
+        yhat = zeros(3)
+        nsplits = 0
+        # fit for each chunk of size base_n
+        for stop in range(start=base_n, stop=n, step=50)
+            nsplits +=1
+            start = stop - base_n + 1
+            xs = x[:,start:stop]
+            Flux.reset!(m)
+            yhat += [m(xs[:,i]) for i=1:base_n][end]
+        end
+        yhat ./= nsplits # fit is average of fit from each chunk
+        Yhat[:,rep] = yhat
+    end    
+    err_nnet[:, :, i] = Yhat - Y .- BC[:,3] 
 end
-BSON.@save "err_nnet_$thisrun.bson" err_nnet BC N MCreps datareps epochs batchsize
-end
-main()
 
+#BSON.@save "err_splitsample_$thisrun.bson" err_nnet
+
+# Compute squared errors
+err_nnet² = abs2.(err_nnet);
+# Compute RMSE for each individual parameter
+rmse_nnet = permutedims(reshape(sqrt.(mean(err_nnet², dims=2)), k, length(N)));
+# Compute RMSE aggregate
+rmse_nnet_agg = mean(rmse_nnet, dims=2);
+
+colors = palette(:default)[1:k]'
+plot(N, rmse_nnet, lw=2, ls=:dash, 
+    lab=map(x -> x * " (NNet)", ["p1" "p2" "p2" "p4" "p5"]),
+    color=colors)
+plot!(N, rmse_nnet_agg, lab="Aggregate (NNet)", c=:black, lw=3, ls=:dash)
+
+savefig("rmse_splitsample_$whichrun.png")
+
+# Compute bias for each individual parameter
+bias_nnet = permutedims(reshape(mean(err_nnet, dims=2), k, length(N)));
+# Compute bias aggregate
+bias_nnet_agg = mean(abs.(bias_nnet), dims=2);
+plot(N, bias_nnet, lw=2, ls=:dash, 
+    lab=map(x -> x * " (NNet)", ["p1" "p2" "p3" "p4" "p5"]),
+    color=colors)
+plot!(N, bias_nnet_agg, lab="Aggregate (abs) (NNet)", c=:black, lw=3, ls=:dash)
+savefig("bias_splitsample_$whichrun.png")
+
+end
+
+main()
