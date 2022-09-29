bc9685cd4207b8200a5a2dd82872a311832fd0a5
Michael Creel <michael.creel@uab.es>
2022-09-29 15:40:04 +0200

    add MLE to MA2 plot

--- MA2/MakePlots.jl
+++ MA2/MakePlots.jl
@@ -2,6 +2,9 @@ using BSON, Plots, Statistics
 
 function MakePlots(whichrun)
 
+# mle
+rmse_mle = [0.0891,0.0606,0.0420, 0.0286,0.0204,0.0145]
+N_mle = [100,200,400,800,1600, 3200]
 # normal fit
 BSON.@load "err_nnet_$whichrun.bson" err_nnet N
 k = size(err_nnet, 1) # Number of parameters
@@ -42,7 +45,11 @@ plot!(N_ss, rmse_ss, lw=2, ls=:dot,
     lab=map(x -> x * " (ss)", ["p1" "p2" "p2" "p4" "p5"]),
     color=colors)
 plot!(N_ss, rmse_ss_agg, lab="Aggregate (ss)", c=:black, lw=3, ls=:dot)
+plot!(N_mle, rmse_mle, lw=2, ls=:solid, 
+    lab=map(x -> x * " (MLE)", ["p1" "p2" "p2" "p4" "p5"]),
+    color=colors)
 savefig("rmse_benchmark_$whichrun.png")
+
 
 plot(N_nnet, bias_nnet, lw=2, ls=:dash, 
     lab=map(x -> x * " (NNet)", ["p1" "p2" "p3" "p4" "p5"]),
