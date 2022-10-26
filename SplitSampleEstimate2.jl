8a11aaafb910540823b3ea1856d13cc1f826e76e
Michael Creel <michael.creel@uab.es>
2022-10-26 10:32:42 +0200

    split sample results

--- SplitSampleEstimate2.jl
+++ SplitSampleEstimate2.jl
@@ -9,34 +9,36 @@ include("DGPs.jl")
 # appears related to https://github.com/JuliaIO/BSON.jl/issues/69
 BSON.@load "models/MA2/mcx2_(n-100).bson" best_model
 Flux.testmode!(best_model)
-
+gpu(best_model)
 function main()
 # General parameters
 test_size = 5_000 # Number of Monte Carlo samples for each sample size
 test_seed = 78
 transform_seed = 1204
 transform_size = 100_000
-dev=cpu
+dev=gpu
 # get the transformation, and set seed to testing
 Random.seed!(transform_seed)
 dgp=Ma2(N=100)
-dtY = data_transform(dgp, transform_size, dev=dev)
+dtY = data_transform(dgp, transform_size, dev=cpu)
 Random.seed!(test_seed)
 k = 2  # number of parameters
 base_n = 100
-Ns = [100, 200, 400, 800, 1600, 3200]  # larger samples to use
+Ns = [100, 200]  # larger samples to use
 results = zeros(6,3)
 # loop over sample sizes
 for i = 1:size(Ns,1)
     n = Ns[i]
     n_splits = n ÷ base_n
     dgp=Ma2(N=n)
-    X, Y = generate(dgp, test_size, dev=dev)
+    X, Y = generate(dgp, test_size, dev=cpu)
     Yhat = Y-Y
     for split = 1:n_splits
         Xs = X[:,:,(split-1)*base_n+1:split*base_n]
         Xs = tabular2conv(Xs)
-        Yhat += StatsBase.reconstruct(dtY, best_model(Xs))
+        Xs |> gpu
+        fit = cpu(best_model(Xs))
+        Yhat += StatsBase.reconstruct(dtY, fit)
     end
     Yhat = cpu(Yhat)
     Yhat ./= n_splits
