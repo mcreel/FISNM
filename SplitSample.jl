37daf2e68e6ecbaf2ad572e8cc02328b973c4bd5
Michael Creel <michael.creel@uab.es>
2022-12-22 14:34:03 +0100

    split sample results, 100, 200 and 400

--- SplitSample.jl
+++ SplitSample.jl
@@ -1,16 +1,27 @@
+global const whichmodelstring="Logit" # MA2, GARCH or Logit
+global const base_n=400
+
+
 using Pkg
 Pkg.activate(".")
 using Flux, CUDA, PrettyTables, Statistics, Term
 using BSON:@load
 include("DGPs.jl")
 
+if whichmodelstring == "MA2"
+	global const whichmodel=Ma2
+elseif	whichmodelstring == "GARCH"
+	global const whichmodel=Garch
+else global const whichmodel=Logit
+end
+
 # for some reason, there is a world age problem if 
 # this is inside the main() function (???)
 # appears related to https://github.com/JuliaIO/BSON.jl/issues/69
-global const base_n=100
-BSON.@load "models/MA2/k3_(n-$base_n).bson" best_model
+
+BSON.@load "models/$whichmodelstring/splitsample_(n-$base_n).bson" best_model
 Flux.testmode!(best_model)
-#gpu(best_model)
+
 function main()
 # General parameters
 test_size = 5_000 # Number of Monte Carlo samples for each sample size
@@ -19,10 +30,10 @@ transform_seed = 1204
 transform_size = 100_000
 # get the transformation, and set seed to testing
 Random.seed!(transform_seed)
-dgp=Ma2(N=base_n)
+dgp=whichmodel(N=base_n)
 dtY = data_transform(dgp, transform_size, dev=cpu)
 Random.seed!(test_seed)
-k = 2  # number of parameters
+k = 3  # number of parameters
 Ns = [Int64(base_n*2^i) for i=0:log(2,3200/base_n)]  # larger samples to use
 results = zeros(size(Ns,1),3)
 # loop over sample sizes
@@ -30,26 +41,23 @@ for i = 1:size(Ns,1)
     n = Ns[i]
     @info "doing n=$n"
     n_splits = n ÷ base_n
-    dgp=Ma2(N=n)
+    dgp=whichmodel(N=n)
     X, Y = generate(dgp, test_size, dev=cpu)
     Yhat = Y-Y
     for split = 1:n_splits
         @info "doing split=$split"
         Xs = X[:,:,(split-1)*base_n+1:split*base_n]
         Xs = tabular2conv(Xs)
- #       Xs |> gpu
-#        fit = cpu(best_model(Xs))
         fit = best_model(Xs)
         Yhat += StatsBase.reconstruct(dtY, fit)
     end
-    #Yhat = cpu(Yhat)
     Yhat ./= n_splits
     err = Yhat - Y
     amae= mean(mean(abs.(err),dims=2))
     armse = mean(sqrt.(mean(abs2.(err),dims=2)))
     results[i,:] = [n, amae, armse]
 end
-println(@green "Split sample average MAE and RMSe, base model n=base_n")
+println(@green "$whichmodelstring model: Split sample average MAE and RMSe, base model n=$base_n")
 return results
 end
 results = main()
--- SplitSample/FSSNM/MA2/MCMC.jl
+++ SplitSample/FSSNM/MA2/MCMC.jl
@@ -37,47 +37,50 @@ function Proposal(current, tuning, cholV)
     current + tuning*cholV'*randn(size(current))
 end
    
-function objective(θ, θhat, S,  dgp, dtY, W)
-    m = (θhat - mean(simstat(θ, dgp, S, dtY),dims=2))[:]
-    dot(m, W, m)
+function objective(θ, θhat, S, dgp, dtY)
+        θbar, Σp = mΣ(θ, dgp, S, dtY)
+        W = inv(Σp)
+        err = θhat - θbar
+        dot(err, W, err)
 end    
 
 
 function main()
     n = 100
+    reps = 10
     dgp = Ma2(N=base_n)
     dtY = maketransform(dgp)
     # initialize args of anonymous function
     dgp = Ma2(N=n)
-    θ = priordraw(dgp, 1)
-    # true params to estimate
-    θtrue = priordraw(dgp, 1)
-    θtcn = simstat(θtrue, dgp, 1, dtY)[:] # the sample statistic
-    display(θtrue)
-    display(θtcn)
-
-
-    # sample using Turing
-    names = ["θ₁", "θ₂"]
-    S = 20
-    covreps = 1000
-    length = 5000
-    burnin = 1000
-    tuning = 15.
-    verbosity = true
-    # the covariance of the proposal (scaled by tuning)
-    junk, Σp = mΣ(θtcn, dgp, covreps, dtY)
-    W = inv(Σp)
-    #Σp = cholesky(Σp)
-    # now do MCMC
-    prior(θ) = insupport(θ) ? 1. : 0.
-    proposal = θ -> Proposal(θ, tuning, Σp)
-    obj = θ -> insupport(θ) ? -objective(θ, θtcn, S, dgp, dtY, W) : Inf
-    chain = mcmc(θtcn, length, burnin, prior, obj, proposal, verbosity)
-    chain = Chains(chain[:,1:2], names)
-    display(plot(chain))
-    display(chain)
-    return chain
+    for rep = 1:reps
+        θ = priordraw(dgp, 1)
+        # true params to estimate
+        θtrue = priordraw(dgp, 1)
+        θtcn = simstat(θtrue, dgp, 1, dtY)[:] # the sample statistic
+        display(θtrue)
+        display(θtcn)
+        # sample using Turing
+        names = ["θ₁", "θ₂"]
+        S = 20
+        covreps = 200
+        length = 2000
+        burnin = 500
+        tuning = 15.
+        verbosity = true
+        # the covariance of the proposal (scaled by tuning)
+        junk, Σp = mΣ(θtcn, dgp, covreps, dtY)
+        W = inv(Σp)
+        Σp = Matrix(cholesky(Σp))
+        # now do MCMC
+        prior(θ) = insupport(θ) ? 1. : 0.
+        proposal = θ -> Proposal(θ, tuning, Σp)
+        obj = θ -> insupport(θ) ? -objective(θ, θtcn, S, dgp, dtY) : Inf
+        chain = mcmc(θtcn, length, burnin, prior, obj, proposal, verbosity)
+        chain = Chains(chain[:,1:2], names)
+        display(plot(chain))
+        display(chain)
+    end    
+    #return chain
 end
 
 
