05b469e7fb7db0a58b838349be12b273919b4235
Michael Creel <michael.creel@uab.es>
2023-03-14 14:35:41 +0100

    plot various JD samples

--- JD/Example.jl
+++ JD/Example.jl
@@ -1,5 +1,12 @@
+using Plots
 include("JDlib.jl")
 
-θ = PriorDraw()
-burnin=100
-data = JDmodel(θ, burnin, rand(1:Int64(1e10)))
+function main()
+    for i = 1:100
+    θ = PriorDraw()
+    burnin=100
+    data = JDmodel(θ, burnin, rand(1:Int64(1e10)))
+    display(plot(data))
+    sleep(0.5)
+    end
+end    
