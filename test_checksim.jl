fea1dfaf73e8ad4a8532b2e1838829bc0763e3bc
JLDC <jonathan.chassot@unisg.ch>
2023-06-05 18:16:20 +0200

    Update checksim

--- test_checksim.jl
+++ test_checksim.jl
@@ -19,13 +19,17 @@ include("NeuralNets/tcn_utils.jl")
 include("MSM/MSM.jl")
 include("MSM/BMSM.jl")
 
-
+specs = (name = "30-20", max_ret = 30, max_rv = 20, max_bv = 20)
+specs = (name = "100-50", max_ret = 100, max_rv = 50, max_bv = 50)
+# specs = (name = "30-20-06_0drop_7layers", max_ret = 30, max_rv = 20, max_bv = 20)
 # Outside of the main() function due to world age issues
 #tcn = BSON.load("models/JD/best_model_ln_bs1024_rv30_capNone.bson")[:best_model];
-tcn = BSON.load("models/JD/best_model_ln_bs1024_30-20.bson")[:best_model];
+tcn = BSON.load("models/JD/best_model_$(specs.name).bson")[:best_model];
+# tcn = BSON.load("models/JD/best_model_ln_bs1024_30-20-06_0drop_7layers.bson")[:best_model];
+Flux.testmode!(tcn);
 
-# Load statistics for standardization
-BSON.@load "statistics_30-20.bson" μs σs
+
+BSON.@load "statistics_$(specs.name).bson" μs σs
 
 @views function preprocess(x) # For X only, don't discard extreme values
     x[:, :, 2:3, :] = log1p.(x[:, :, 2:3, :]) # Log RV and BV
@@ -76,17 +80,26 @@ reps = 1000
 
 d = generate(Float32.(θ̂ₓ), dgp, reps)
 # Check and reject extreme samples
-idx = maximum(abs, d[1, :, :], dims=2) .≤ 50
-idx2 = mean(d[2, :, :], dims=2) .≤ 50 # Mean RV under threshold
-idx3 = mean(d[3, :, :], dims=2) .≤ 50 # Mean BV under threshold
+# idx = maximum(abs, d[1, :, :], dims=2) .≤ 50
+# idx2 = mean(d[2, :, :], dims=2) .≤ 50 # Mean RV under threshold
+# idx3 = mean(d[3, :, :], dims=2) .≤ 50 # Mean BV under threshold
 
-idx = idx .& idx2 .& idx3 |> vec
-@info "Discarding $(reps - sum(idx)) observations"
-d = d[:, idx, :]
+# idx = idx .& idx2 .& idx3 |> vec
+# @info "Discarding $(reps - sum(idx)) observations"
+# d = d[:, idx, :]
 
 means = mean(d, dims=[2,3]) |> vec
 
+# means = mean(d, dims=2)
 
+# histogram(means[1, :, :] |> vec, lab="Simulated", nbins=100)
+# vline!([mean(df.rets)], lab="SP500")
+
+# histogram(means[2, :, :] |> vec, lab="Simulated", nbins=100)
+# vline!([mean(df.rv)], lab="SP500")
+
+# histogram(means[3, :, :] |> vec, lab="Simulated", nbins=100)
+# vline!([mean(df.bv)], lab="SP500")
 
 # m = mean(d[1, :, :], dims=2) |> vec
 # histogram(m, lab="")
